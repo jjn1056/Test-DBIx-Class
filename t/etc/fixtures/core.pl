7168d753a926f7b79bf4c3e0796b0bd26f4b84f3
carlvincent <github@carlvincent.co.uk>
2009-09-16 11:48:40 +0100

    Modified core fixture to contain data for two tables. Modified load-fixtures test to expect this result. Test fails. Hash::Merge::merge at line 432 of Test::DBIx::Class.pm doesn't cope with merging an arrayref of hashrefs (multi table fixture) with plain hashref (single table fixture)

--- t/etc/fixtures/core.pl
+++ t/etc/fixtures/core.pl
@@ -1,24 +1,36 @@
-$VAR1 = {
-  'Person' => [
-    [
-      'name',
-      'age',
-      'email'
+$VAR1 = [
+  {
+    'Person' => [
+      [
+        'name',
+        'age',
+        'email'
+      ],
+      [
+        'John',
+        '40',
+        'john@nowehere.com'
+      ],
+      [
+        'Vincent',
+        '15',
+        'vincent@home.com'
+      ],
+      [
+        'Vanessa',
+        '35',
+        'vanessa@school.com'
+      ]
     ],
-    [
-      'John',
-      '40',
-      'john@nowehere.com'
+  },
+  {
+    'Company' => [
+      [
+        'name',
+      ],
+      [
+        'Acme',
+      ],
     ],
-    [
-      'Vincent',
-      '15',
-      'vincent@home.com'
-    ],
-    [
-      'Vanessa',
-      '35',
-      'vanessa@school.com'
-    ]
-  ]
-};
+  },
+];
